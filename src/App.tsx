ff58bd6801f86ebf66647f4afcecf6e331adfa48
Chris K <cl_killen@hotmail.com>
2022-01-06 04:34:02 -0500

    done for today

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Finished!!! one more test 
+           Finished for real :)
         </p>
         <a
           className="App-link"
