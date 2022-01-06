74d9b88bc77fc39901b01e8b60fef85c29e5cd4c
Chris K <cl_killen@hotmail.com>
2022-01-06 03:25:56 -0500

    did this mess it up

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Finished!!! 
+          Finished!!! one more test 
         </p>
         <a
           className="App-link"
