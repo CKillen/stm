9020fc01b78d78f0545aef6e221ed2dff3b7b39d
Chris K <cl_killen@hotmail.com>
2022-01-06 00:45:11 -0500

    Test auto

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          A big test
+          Let's test the autodeployment
         </p>
         <a
           className="App-link"
